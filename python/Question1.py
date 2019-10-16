100b823142ef84920824110acd9d26608237f437
sukalp1 <56125072+sukalp1@users.noreply.github.com>
2019-10-16 22:22:12 +0200

    Update Question1.py

--- python/Question1.py
+++ python/Question1.py
@@ -1,5 +1,14 @@
-# modify this function, and create other functions below as you wish
 def question01(initialLevelOfDebt, interestPercentage, repaymentPercentage):
     # modify and then return the variable below
-    answer = -5
-    return answer
+  answer = 0.1 * initialLevelOfDebt;
+  debt = initialLevelOfDebt;
+  emi = initialLevelOfDebt * (repaymentPercentage / 100) if initialLevelOfDebt * (repaymentPercentage / 100) > 50 else 50;
+  while debt > 0:
+    debt = round(debt * 1.05, 2)
+    if debt < emi:
+      answer = answer + debt;
+      debt = 0;
+      break;
+    debt = debt - emi;
+    answer = answer + emi;
+  return int(answer / 10) * 10;
